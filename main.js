b1e2613952032a50dd8154a426ae3cc15b1bd3d1
Alyona <polova_alyona0907@ukr.net>
2024-01-02 15:19:54 +0100

    update main

--- main.js
+++ main.js
@@ -26,15 +26,15 @@ function generatePlayfield() {
   playfield = new Array(PLAYFIELD_ROWS)
     .fill()
     .map(() => new Array(PLAYFIELD_COLUMS).fill(0));
-  console.log(playfield);
+  //   console.log(playfield);
 }
 
 function generateTetromino() {
   const nameTetro = "o";
   const matrixTetro = TETROMINOES[nameTetro];
 
-  const columnTetro = 5;
-  const rowTetro = 3;
+  const columnTetro = 3;
+  const rowTetro = 5;
 
   tetromino = {
     name: nameTetro,
@@ -46,8 +46,9 @@ function generateTetromino() {
 
 generatePlayfield();
 generateTetromino();
+
 const cells = document.querySelectorAll(".tetris div");
-console.log(cells);
+// console.log(cells);
 
 function drawTetromino() {
   const name = tetromino.name;
@@ -65,3 +66,58 @@ function drawTetromino() {
 }
 
 drawTetromino();
+
+function draw() {
+  cells.forEach((cell) => cell.removeAttribute("class"));
+  drawTetromino();
+}
+
+document.addEventListener("keydown", onKeyDown);
+
+function onKeyDown(e) {
+  console.log(e);
+  switch (e.key) {
+    case "ArrowDown":
+      moveTetrominoDown();
+      break;
+    case "ArrowLeft":
+      console.log(e.key);
+      moveTetrominoLeft();
+      break;
+    case "ArrowRight":
+      console.log(e.key);
+      moveTetrominoRight();
+      break;
+  }
+  draw();
+}
+
+function moveTetrominoDown() {
+  tetromino.row += 1;
+}
+// function moveTetrominoLeft() {
+//   if (isOutsideOfGameBoard()) {
+//     tetromino.column -= 1;
+//   }
+// }
+// function moveTetrominoRight() {
+//   if (isOutsideOfGameBoard()) {
+//     tetromino.column += 1;
+//   }
+// }
+
+function isOutsideOfGameBoard() {
+  const matrixSize = tetromino.matrix.length;
+
+  for (let row = 0; row < matrixSize; row++) {
+    for (let column = 0; column < matrixSize; column++) {
+      if (
+        tetromino.column + column < 1 ||
+        tetromino.column + column >= PLAYFIELD_COLUMS - 1
+      ) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
